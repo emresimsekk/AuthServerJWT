6364a43783107cd3cbba80af2c345b6e49c9a2fe
Emre <dev.simsek.emre@gmail.com>
2021-01-09 01:52:44 +0300

    Generic Repo- Dtos global errors

---
+++ AuthServerJWT.Core/Entity/UserRefreshToken.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuthServerJWT.Core.Entity
+{
+    public class UserRefreshToken
+    {
+        public string UserID { get; set; }
+        public string Code { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+}
---
+++ AuthServerJWT.Shared/Dtos/ErrorDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuthServerJWT.Shared.Dtos
+{
+    public class ErrorDto
+    {   
+        public List<String> Errors { get;private set; }
+
+        public bool IsShow { get; private set; }
+        public ErrorDto()
+        {
+            Errors = new List<string>();
+        }
+        public ErrorDto(string error, bool isShow)
+        {
+            Errors.Add(error);
+            isShow = true;
+        }
+        public ErrorDto(List<string> errors, bool isShow)
+        {
+            Errors = errors;
+            IsShow = isShow;
+        }
+    }
+}
